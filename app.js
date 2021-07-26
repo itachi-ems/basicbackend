97f7b232969abd2acf844e3c12b323db3af53a16
itachi-ems <jshahharsh@gmail.com>
2021-07-26 19:16:07 +0530

    swagger and logging done

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express =require('express');
 const userRoute=require('./Routes/userRoutes');
+swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./swagger.json');
+const log4js = require('log4js');
 
 const app=express();
 
@@ -13,7 +16,21 @@ app.use(function(req, res, next) {
 });
 
 
+//logging
+log4js.configure({
+    appenders: { fileAppender: { type: 'file', filename: './logs/all.log' } },
+    categories: { default: { appenders: ['fileAppender'], level: 'info' } }
+});
+
+// Create the logger
+const logger = log4js.getLogger();
+
+// Log a message
+logger.info("hey");
+
+
 app.use('/api/user',userRoute);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 
