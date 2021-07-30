a82cff4b198b7bb36993114db4789c566d1d58b9
itachi-ems <jshahharsh@gmail.com>
2021-07-30 12:20:05 +0530

    validation changes

--- models/user.js
+++ models/user.js
@@ -14,7 +14,15 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   User.init({
-    email: {type:DataTypes.STRING,allowNull:false},
+    email: {type:DataTypes.STRING,allowNull:false,
+      validate: {
+      notEmpty: true,
+      notIn: [["Demo", "Trial"]],
+      len: {
+        args: [1, 10],
+        msg: "Password must be between 2 to 16", //custom message
+      },
+    }},
     password: {type:DataTypes.STRING,allowNull:false}
   }, {
     sequelize,
